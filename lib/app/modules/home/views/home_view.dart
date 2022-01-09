28f2b4c25326066589d87891fe2e955817196061
Anas Misbahuddin <anas.misbahuddin4869@gmail.com>
2022-01-09 11:29:09 +0800

    set pict trending furniture

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,248 +10,250 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF6F6F6),
-      body: ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              height: 60,
+              width: Get.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Furniture Shop",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF202020),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Get unique furniture from home",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color(0xFFB2B2B2),
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Image.asset("assets/icons/cart.png"))
+                ],
+              ),
             ),
-            height: 60,
-            width: Get.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  label: Text("Search unique furniture ..."),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: InputBorder.none,
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            TitleSection(
+              title: "Furniture Shop",
+              onTap: () {
+                Get.toNamed(Routes.CATEGORY);
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                padding: EdgeInsets.only(left: 20),
+                child: Row(
                   children: [
-                    Text(
-                      "Furniture Shop",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF202020),
+                    CategoryItem(
+                      title: "Living Room",
+                      icon: "assets/icons/livingroom.png",
+                    ),
+                    CategoryItem(
+                      title: "Bath Room",
+                      icon: "assets/icons/bathroom.png",
+                    ),
+                    CategoryItem(
+                      title: "Hole",
+                      icon: "assets/icons/hole.png",
+                    ),
+                    CategoryItem(
+                      title: "Kitchen",
+                      icon: "assets/icons/kitchen.png",
+                    ),
+                    CategoryItem(
+                      title: "Workspace",
+                      icon: "assets/icons/workspace.png",
+                    ),
+                    CategoryItem(
+                      title: "Bed Room",
+                      icon: "assets/icons/bedroom.png",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TitleSection(
+              title: "Today's Promo",
+              onTap: () {
+                print("clicked");
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                padding: EdgeInsets.only(left: 20),
+                child: Row(
+                  children: [
+                    PromoItem(
+                      discount: "50%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-1.png",
+                    ),
+                    PromoItem(
+                      discount: "60%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-2.png",
+                    ),
+                    PromoItem(
+                      discount: "55%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-3.png",
+                    ),
+                    PromoItem(
+                      discount: "40%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property4.png",
+                    ),
+                    PromoItem(
+                      discount: "35%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-5.png",
+                    ),
+                    PromoItem(
+                      discount: "60%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-6.png",
+                    ),
+                    PromoItem(
+                      discount: "50%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-7.png",
+                    ),
+                    PromoItem(
+                      discount: "20%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-8.png",
+                    ),
+                    PromoItem(
+                      discount: "10%OFF",
+                      date: "30 September 2019",
+                      image: "assets/images/property-9.png",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TitleSection(
+              title: "Trending Furniture",
+              onTap: () {
+                print("clicked");
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                padding: EdgeInsets.only(left: 20),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 15),
+                      width: 210,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/trending-1.png"),
+                            fit: BoxFit.cover),
                       ),
                     ),
-                    SizedBox(
-                      height: 5,
+                    Container(
+                      margin: EdgeInsets.only(right: 15),
+                      width: 210,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/trending-2.png"),
+                            fit: BoxFit.cover),
+                      ),
                     ),
-                    Text(
-                      "Get unique furniture from home",
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Color(0xFFB2B2B2),
+                    Container(
+                      margin: EdgeInsets.only(right: 15),
+                      width: 210,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/trending-1.png"),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 10),
+                      width: 210,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/trending-2.png"),
+                            fit: BoxFit.cover),
                       ),
                     ),
                   ],
                 ),
-                IconButton(
-                    onPressed: () {},
-                    icon: Image.asset("assets/icons/cart.png"))
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
-              decoration: InputDecoration(
-                label: Text("Search unique furniture ..."),
-                fillColor: Colors.white,
-                filled: true,
-                border: InputBorder.none,
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 35,
-          ),
-          TitleSection(
-            title: "Furniture Shop",
-            onTap: () {
-              Get.toNamed(Routes.CATEGORY);
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              padding: EdgeInsets.only(left: 20),
-              child: Row(
-                children: [
-                  CategoryItem(
-                    title: "Living Room",
-                    icon: "assets/icons/livingroom.png",
-                  ),
-                  CategoryItem(
-                    title: "Bath Room",
-                    icon: "assets/icons/bathroom.png",
-                  ),
-                  CategoryItem(
-                    title: "Hole",
-                    icon: "assets/icons/hole.png",
-                  ),
-                  CategoryItem(
-                    title: "Kitchen",
-                    icon: "assets/icons/kitchen.png",
-                  ),
-                  CategoryItem(
-                    title: "Workspace",
-                    icon: "assets/icons/workspace.png",
-                  ),
-                  CategoryItem(
-                    title: "Bed Room",
-                    icon: "assets/icons/bedroom.png",
-                  ),
-                ],
-              ),
+            SizedBox(
+              height: 20,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TitleSection(
-            title: "Today's Promo",
-            onTap: () {
-              print("clicked");
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              padding: EdgeInsets.only(left: 20),
-              child: Row(
-                children: [
-                  PromoItem(
-                    discount: "50%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-1.png",
-                  ),
-                  PromoItem(
-                    discount: "60%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-2.png",
-                  ),
-                  PromoItem(
-                    discount: "55%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-3.png",
-                  ),
-                  PromoItem(
-                    discount: "40%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property4.png",
-                  ),
-                  PromoItem(
-                    discount: "35%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-5.png",
-                  ),
-                  PromoItem(
-                    discount: "60%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-6.png",
-                  ),
-                  PromoItem(
-                    discount: "50%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-7.png",
-                  ),
-                  PromoItem(
-                    discount: "20%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-8.png",
-                  ),
-                  PromoItem(
-                    discount: "10%OFF",
-                    date: "30 September 2019",
-                    image: "assets/images/property-9.png",
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TitleSection(
-            title: "Trending Furniture",
-            onTap: () {
-              print("clicked");
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              padding: EdgeInsets.only(left: 20),
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(right: 15),
-                    width: 210,
-                    height: 130,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://picsum.photos/id/866/200"),
-                            fit: BoxFit.cover)),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 15),
-                    width: 210,
-                    height: 130,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://picsum.photos/id/867/200"),
-                            fit: BoxFit.cover)),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 15),
-                    width: 210,
-                    height: 130,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://picsum.photos/id/868/200"),
-                            fit: BoxFit.cover)),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 10),
-                    width: 210,
-                    height: 130,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://picsum.photos/id/869/200"),
-                            fit: BoxFit.cover)),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
